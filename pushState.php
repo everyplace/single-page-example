98f8b63bcad15324468d1a9f9730d39a29d5a54e
Erin Sparling <erin@everyplace.net>
2012-11-20 21:52:31 -0500

    Commented, slightly.

--- pushState.php
+++ pushState.php
@@ -4,17 +4,18 @@
 	$json = json_decode(file_get_contents('pages.json'));
 	$numberOfPages = count((array)$json);
 
+	//use get just the first part of the query parameter
 	if(isset($_GET)) {
+		//should be a regex
 		$currentPage = explode(".",$_GET['page']);
 		$currentPage = $currentPage[0];
 		echo $currentPage;
 		
+		//select the current page from the json object
 		$currentPageData = $json->$currentPage;
-		
 		$title = $currentPageData->title;
 		$content = $currentPageData->content;
 	}
-
 
 ?>
 <!DOCTYPE html>
@@ -25,17 +26,25 @@
 		<script>
 			$(document).ready(function(){
 		
-		
+				//find each list item
 				$("#pages>li").each(function(index, item){
 					$item = $(item);
+					
+					//the dynamically created id originally came from the json object
 					var id = $item.attr("id");
 					
 					$item.click(function(item){
-					history.pushState("", "id", id + ".html");
+					
+						//when clicked, update the url
+						history.pushState("", "id", id + ".html");
+						
+						//get the new content
 						$.ajax({
 						  url: "pages.json",
 						  dataType: 'json',
 						  success: function(data){
+						  
+						  	//update the current html structure with the new content
 							$("#title").html(data[id].title);
 							$("#content").html(data[id].content);
 						  }
